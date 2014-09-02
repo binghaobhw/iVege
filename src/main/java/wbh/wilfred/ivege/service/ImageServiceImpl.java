dd171e574d5ddf1972c0f5c3ce05009cb3b1ee47
wilfredwang <scorpio147wbh@gmail.com>
2014-09-02 20:25:52 +0800

    multipart upload dependency

--- src/main/java/wbh/wilfred/ivege/service/ImageServiceImpl.java
+++ src/main/java/wbh/wilfred/ivege/service/ImageServiceImpl.java
@@ -1,16 +1,25 @@
 package wbh.wilfred.ivege.service;
 
-import java.security.MessageDigest;
+import org.springframework.stereotype.Service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Service
 public class ImageServiceImpl implements ImageService {
     @Override
     public String addImage(byte[] bytes) {
-        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+        MessageDigest messageDigest;
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException();
+        }
         messageDigest.update(bytes);
         byte[] digest = messageDigest.digest();
         StringBuilder hash = new StringBuilder();
-        for (int i = 0 ; i < digest.length; ++i) {
-            hash.append(Integer.toString((digest[i] & 0xff) + 0x100,
+        for (byte aDigest : digest) {
+            hash.append(Integer.toString((aDigest & 0xff) + 0x100,
                     16).substring(1));
         }
         return hash.toString();
